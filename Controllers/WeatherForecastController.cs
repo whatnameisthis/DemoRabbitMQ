354dd18b362c38c9659ec1917b5f660c6449a068
Rajan Kumar Singh <Rajan.K.Singh@de.ey.com>
2022-02-08 18:50:34 +0100

    initial commit

---
+++ Controllers/WeatherForecastController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoRabbitMQ.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LocationController : ControllerBase
+    {
+        [HttpPost]
+        public void Post([FromBody] WeatherForecast location)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "TestQueue",
+                                     durable: false,
+                                     exclusive: false,
+                                     autoDelete: false,
+                                     arguments: null);
+
+                string message = "Latitude: " + location.TemperatureC + ", Longitude: " + location.TemperatureF + " and Time: " + location.Date;
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                     routingKey: "TestQueue",
+                                     basicProperties: null,
+                                     body: body);
+            }
+        }
+    }
+
+   
+}
